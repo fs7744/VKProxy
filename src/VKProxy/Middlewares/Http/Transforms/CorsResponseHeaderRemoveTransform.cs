1fa2698c51720b46b7776beaa17fd443e5f6f4d1
Victor Qu <Victor.X.Qu@newegg.com>
2025-06-25 16:08:45 +0800

    CorsFunc

---
+++ src/VKProxy/Middlewares/Http/Transforms/CorsResponseHeaderRemoveTransform.cs
@@ -0,0 +1,54 @@
+﻿namespace VKProxy.Middlewares.Http.Transforms;
+
+internal class CorsResponseHeaderRemoveTransform : ResponseTransform, ITransformProvider
+{
+    public void Apply(TransformBuilderContext context)
+    {
+        context.ResponseTransforms.Add(this);
+    }
+
+    public override ValueTask ApplyAsync(ResponseTransformContext context)
+    {
+        var items = context.HttpContext.Items;
+        var respH = context.HttpContext.Response.Headers;
+        if (items.ContainsKey("AccessControlAllowOrigin"))
+        {
+            var d = respH.AccessControlAllowOrigin;
+            if (d.Count > 1)
+            {
+                respH.AccessControlAllowOrigin = d.First();
+            }
+
+            d = respH.AccessControlAllowHeaders;
+            if (d.Count > 1)
+            {
+                respH.AccessControlAllowHeaders = d.First();
+            }
+
+            d = respH.AccessControlAllowMethods;
+            if (d.Count > 1)
+            {
+                respH.AccessControlAllowMethods = d.First();
+            }
+
+            d = respH.AccessControlAllowCredentials;
+            if (d.Count > 1)
+            {
+                respH.AccessControlAllowCredentials = d.First();
+            }
+
+            d = respH.AccessControlMaxAge;
+            if (d.Count > 1)
+            {
+                respH.AccessControlMaxAge = d.First();
+            }
+
+            d = respH.AccessControlExposeHeaders;
+            if (d.Count > 1)
+            {
+                respH.AccessControlExposeHeaders = d.First();
+            }
+        }
+        return default;
+    }
+}
