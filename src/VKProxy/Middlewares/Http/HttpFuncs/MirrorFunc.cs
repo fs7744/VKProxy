35f70f3d9c268b882444730ca8b62727f24ffc72
Victor Qu <Victor.X.Qu@newegg.com>
2025-07-04 15:25:52 +0800

    remove using

--- src/VKProxy/Middlewares/Http/HttpFuncs/MirrorFunc.cs
+++ src/VKProxy/Middlewares/Http/HttpFuncs/MirrorFunc.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
-using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using VKProxy.Config;
@@ -9,7 +8,6 @@ using VKProxy.Core.Loggers;
 using VKProxy.Features;
 using VKProxy.LoadBalancing;
 using VKProxy.Middlewares.Http.Transforms;
-using static VKProxy.Core.Adapters.HttpApplication;
 
 namespace VKProxy.Middlewares.Http.HttpFuncs;
 
