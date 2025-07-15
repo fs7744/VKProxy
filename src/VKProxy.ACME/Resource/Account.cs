5628a944f1f4c0084b095d315c67c43b4b2579ee
Victor Qu <Victor.X.Qu@newegg.com>
2025-07-15 17:13:14 +0800

    Account

---
+++ src/VKProxy.ACME/Resource/Account.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VKProxy.ACME.Resource;
+
+public enum AccountStatus
+{
+    /// <summary>
+    /// The valid status.
+    /// </summary>
+    Valid,
+
+    /// <summary>
+    /// The deactivated status, initiated by client.
+    /// </summary>
+    Deactivated,
+
+    /// <summary>
+    /// The revoked status, initiated by server.
+    /// </summary>
+    Revoked,
+}
+
+public class Account
+{
+    public AccountStatus? Status { get; set; }
+
+    public IList<string>? Contact { get; set; }
+    public bool? TermsOfServiceAgreed { get; set; }
+
+    public Uri? Orders { get; set; }
+
+    public object? ExternalAccountBinding { get; set; }
+}
