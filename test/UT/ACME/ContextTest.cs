0adf463da747c37e5757e6b2a1514c973ddb4027
Victor Qu <Victor.X.Qu@newegg.com>
2025-07-17 10:57:29 +0800

    test

--- test/UT/ACME/ContextTest.cs
+++ test/UT/ACME/ContextTest.cs
@@ -1,31 +1,32 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using VKProxy.ACME;
+﻿//using Microsoft.Extensions.DependencyInjection;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading;
+//using System.Threading.Tasks;
+//using VKProxy;
+//using VKProxy.ACME;
 
-namespace UT.ACME;
+//namespace UT.ACME;
 
-public class ContextTest
-{
-    [Fact]
-    public async Task DoTest()
-    {
-        var services = new ServiceCollection();
-        services.AddACME();
-        var p = services.BuildServiceProvider();
-        var client = p.GetRequiredService<IAcmeClient>();
-        var context = p.GetRequiredService<IAcmeContext>();
-        await context.InitAsync(WellKnownServers.LetsEncryptStagingV2, CancellationToken.None);
-        //context.NewAccountAsync();
-        //var nonce = await client.NewNonceAsync(context.Directory.NewNonce, CancellationToken.None);
-    }
+//public class ContextTest
+//{
+//    [Fact]
+//    public async Task DoTest()
+//    {
+//        var services = new ServiceCollection();
+//        services.AddACME();
+//        var p = services.BuildServiceProvider();
+//        var client = p.GetRequiredService<IAcmeClient>();
+//        var context = p.GetRequiredService<IAcmeContext>();
+//        await context.InitAsync(WellKnownServers.LetsEncryptStagingV2, CancellationToken.None);
+//        //var nonce = await client.NewNonceAsync(context.Directory.NewNonce, CancellationToken.None);
+//    }
 
-    [Fact]
-    public async Task AccountKeyTest()
-    {
-    }
-}
+//    [Fact]
+//    public async Task AccountKeyTest()
+//    {
+//        await VKProxyHost.RunAsync("acme account update -k D:\\code\\github\\VKProxy\\src\\VKProxy.Cli\\bin\\Debug\\net9.0\\key -c mailto:xxx@xxx.com,mailto:test@xxx.com --debug".Split(' '));
+//    }
+//}
