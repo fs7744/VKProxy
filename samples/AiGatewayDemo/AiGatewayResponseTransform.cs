45b8bcbb3c896a6dd146307eec62819c1aa954de
Victor.x.qu <fs7744@hotmail.com>
2026-01-08 16:53:52 +0800

    remove test code

--- samples/AiGatewayDemo/AiGatewayResponseTransform.cs
+++
@@ -1,9 +0,0 @@
-﻿using VKProxy.Middlewares.Http.Transforms;
-
-internal class AiGatewayResponseTransform : ResponseTransform
-{
-    public override ValueTask ApplyAsync(ResponseTransformContext context)
-    {
-        throw new NotImplementedException();
-    }
-}
